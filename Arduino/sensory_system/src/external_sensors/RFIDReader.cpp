786f3af9b887a963c1fbdce0f4730144ab6e904a
Cyril Marx <marxcyril@gmail.com>
2022-10-01 12:18:11 +0200

    Started files for RFID reader implementation.

---
+++ Arduino/sensory_system/src/external_sensors/RFIDReader.cpp
@@ -0,0 +1,9 @@
+/**********************************************************************
+ * UltrasonicDistanceModule.cpp
+ * 
+ * Implementation of the ultrasonic distance measuring class.
+ * 
+ * Author: Cyril Marx
+ * Created: September 2022
+ **********************************************************************/
+
---
+++ Arduino/sensory_system/src/external_sensors/RFIDReader.hpp
@@ -0,0 +1,23 @@
+/**********************************************************************
+ * UltrasonicDistance.hpp
+ * 
+ * A class representing a single ultrasonic distance measuring module.
+ * Extends the Module base class.
+ * 
+ * Author: Cyril Marx
+ * Created: September 2022
+ **********************************************************************/
+
+#ifndef RFIDREADER_HPP
+#define RFIDREADER_HPP
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include "../base_libraries/Module.hpp"
+
+class RFIDReader : public Module
+{
+
+};
+
+#endif //RFIDREADER_HPP
