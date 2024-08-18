5c57f5973347039eb9f5c440d8d0e0d638543dbe
Cyril Marx <marxcyril@gmail.com>
2024-08-18 17:42:23 +0200

    Started implementation of MQTT middleware.

---
+++ UtilityScripts/mqtt_test_publisher.py
@@ -0,0 +1,23 @@
+"""
+A test script to publish data to the locally running MQTT broker.
+Command to start broker:
+docker run -it -p 1883:1883 -p 9001:9001 -v /opt/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v /opt/mosquitto/data:/mosquitto/data -v /opt/mosquitto/log:/mosquitto/log eclipse-mosquitto --restart unless-stopped
+"""
+
+import paho.mqtt.client as mqtt
+
+if __name__ == "__main__":
+  address = "localhost"
+  port = 1883
+  print(f"[Info] Connecting to MQTT broker on {address}:{port}")
+  mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+  mqttc.connect(address, port, 60)
+  mqttc.loop_start()
+
+  print("[Info] Starting message publishing.")
+  i = 0
+  while True:
+      i += 1
+      mqttc.publish("testtopic/test_i", i)
+
+  mqttc.loop_stop()
---
+++ UtilityScripts/mqtt_test_subscriber.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("testtopic/test_i")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+if __name__ == "__main__":
+  address = "localhost"
+  port = 1883
+  print(f"[Info] Connecting to MQTT broker on {address}:{port}")
+  mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+  mqttc.on_connect = on_connect
+  mqttc.on_message = on_message
+
+  mqttc.connect(address, port, 60)
+
+  # Blocking call that processes network traffic, dispatches callbacks and
+  # handles reconnecting.
+  # Other loop*() functions are available that give a threaded interface and a
+  # manual interface.
+  print("[Info] Receiving messages now.")
+  mqttc.loop_forever()
